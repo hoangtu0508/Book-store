c4cf4917a9295017e8e2203587ea04f5b5731b68
TRƯƠNG HOÀNG TÚ <73875963+hoangtu0508@users.noreply.github.com>
2024-05-06 19:58:53 +0700

    Delete src directory

--- src/pages/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import Cookies from "js-cookie";
-
-const Logout = () => {
-  Cookies.remove("authToken");
-  Cookies.remove("isAdmin");
-  window.location = "http://localhost:3000";
-};
-
-export default Logout;
